b439c0a171ce4373a995bd995f885fae3352061a
Shaheer <shaheerkashif345@gmail.com>
2024-01-07 11:34:49 +0500

    Changed Prog3 Feature

--- prog3.py
+++ prog3.py
@@ -1,5 +1,7 @@
 print("Adding Two Numbers!")
 a = int(input("Enter First Number: "))
 b = int(input("Enter Second Number: "))
-c = a + b
-print("The Result is:",c)
+print("The Addition is:",a+b)
+print("The Subtraction is:",a-b)
+print("The Multiplication is:",a*b)
+print("The Division is:",a/b)
