eaf0a8d01484cd62626ec2bc6a141f203be72eb1
Shaheer <shaheerkashif345@gmail.com>
2024-01-25 12:52:34 +0500

    Random Yapping

--- prog3.py
+++ prog3.py
@@ -4,4 +4,4 @@ b = int(input("Enter Second Number: "))
 print("The Addition is:",a+b)
 print("The Subtraction is:",a-b)
 print("The Multiplication is:",a*b)
-print("The Division is:",a/b)
+print("The Division is:",a/b)fsafsfsfsafsfsa
